5d16c65aa40c3435bc15015a39f2868e2e97e6b4
HierComsDatBoi <dainius.slekonis@gmail.com>
2024-10-08 18:40:38 +0300

    modified all books page, created booccard component

--- client/src/components/BookCard.jsx
+++ client/src/components/BookCard.jsx
@@ -1,28 +1,31 @@
+const BookCard = ({ value }) => {
 
+  const shortString = (str, num) => {
+    return str.slice(0, num) + '...'
+  }
 
+  return (
+    <>
+      <div>
 
-const BookCard = ({data}) => {
-  return ( data ? 
-  <>
-    <div><img src={data.imageUrl} alt={data.title} /></div>
-    <div>
-      <h2>{data.title}</h2>
-      <p>{data.description}</p>
-      <ul>
-        <li><strong>Author: </strong>{data.author}</li>
-        <li><strong>Genres: </strong>
-          {
-            data.genres.map((genre, i) => <span key={i}>{genre} </span>)
-          }
-        </li>
-        <li><strong>Pages: </strong>{data.pages}</li>
-        <li><strong>Publish Date: </strong>{data.publishDate}</li>
-        <li><strong>Rating: </strong>{data.rating}</li>
-        <li><strong>Available Copies: </strong>{data.amountOfCopies}</li>
-      </ul>
-    </div>
-          </> : <div>Loading...</div>
-   );
+        <div><img src={value.imageUrl} alt={value.title} /></div>
+        <div>
+          <h2>{value.title}</h2>
+          <p>{shortString(value.description, 150)}</p>
+          <ul>
+            <li><strong>Author: </strong>{value.author}</li>
+            <li><strong>Genres: </strong>
+              {
+                value.genres.map((genre, i) => <span key={i}>{genre} </span>)
+              }
+            </li>
+            <li><strong>Publish Date: </strong>{value.publishDate}</li>
+          </ul>
+          <button onClick={() => window.open(`/AllBooks/${value._id}`, '_blank')}>More Info</button>
+        </div>
+      </div>
+    </>
+  );
 }
- 
+
 export default BookCard;
--- client/src/pages/AllBooks.jsx
+++ client/src/pages/AllBooks.jsx
@@ -1,33 +1,40 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import BookCard from '../components/BookCard';
 
 const StyledSection = styled.section`
-background-color: black;
-color: white;
-button{
-  border:none;
-  border-radius: 5px;
-  background-color: gray;
+  background-color: black;
   color: white;
-  padding: 10px 15px;
-  &:hover{
-    cursor: pointer;
-    background-color: orange;
+
+  button {
+    border: none;
+    border-radius: 5px;
+    background-color: gray;
+    color: white;
+    padding: 10px 15px;
+
+    &:hover {
+      cursor: pointer;
+      background-color: orange;
+    }
   }
-}
->div{
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  max-width: 80vw;
-  gap:30px;
-  margin: 0 auto;
-  >div>div{
-  margin: 5px 0;
+
+  img {
+    height: 50vh;
   }
-  img{
-    height: 40vh;
+
+  >div {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 50px;
+    justify-items: center;
+    margin: 0 10vw;
+    >div{
+      display: flex;
+      width: 40vw;
+      gap: 20px;
+    }
   }
-}
   `;
 
 const AllBooks = () => {
@@ -51,25 +58,13 @@ const AllBooks = () => {
           <div>
             {
               books.map(book =>
-                <div key={book._id}>
-                  <h3>{book.title}</h3>
-                  <div><img src={book.imageUrl} alt={book.title} /></div>
-                  <div><strong>Genres: </strong>
-                    {
-                      book.genres.map((genre, i) =>
-                        <span key={i}>{genre} </span>
-                      )
-                    }
-                  </div>
-                  <button onClick={() => window.open(`/AllBooks/${book._id}`, '_blank')}>More Info</button>
-                </div>
+                <BookCard key={book._id} value={book} />
               )
             }
           </div>
       }
     </StyledSection>
   );
-
 }
 
 export default AllBooks;
